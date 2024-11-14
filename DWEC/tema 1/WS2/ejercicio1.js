774e0465b35036e066e3f9a19b25bcad888945e7
Roberto España <robertoz1777@gmail.com>
2024-11-14 14:44:28 +0100

    Delete DWEC/tema 1/WS2 directory

--- DWEC/tema 1/WS2/ejercicio1.js
+++
@@ -1,8 +0,0 @@
-let anoActual = new Date().getFullYear(); 
-let anioNacimiento = parseInt(prompt("Ingresa tu año de nacimiento")); 
-
-let edadEsteAnio = anoActual - anioNacimiento; 
-let edadSiguienteAnio = edadEsteAnio + 1; 
-
-alert("La edad es: " + edadEsteAnio + " o " + edadSiguienteAnio + "."); 
-console.log("La edad es: " + edadEsteAnio + " o " + edadSiguienteAnio + ".");
