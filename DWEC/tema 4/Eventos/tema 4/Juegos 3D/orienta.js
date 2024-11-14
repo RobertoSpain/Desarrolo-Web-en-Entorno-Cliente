0888eabf8c3ea51db46947234a118a1087f5a9f5
Roberto España <robertoz1777@gmail.com>
2024-11-14 14:48:47 +0100

    Delete DWEC/tema 4/Eventos directory

--- DWEC/tema 4/Eventos/tema 4/Juegos 3D/orienta.js
+++
@@ -1,4 +0,0 @@
-function cambiarColor()
-{
-    document.getElementById
-}
--- DWEC/tema 4/Eventos/tema 4/relacion 3/Rotacion1.js
+++
@@ -1,5 +0,0 @@
-// Detecta el evento de rotación del dispositivo
-window.addEventListener('deviceorientation', (event) => {
-    const alpha = event.alpha;
-        document.getElementById("myDIV").style.transform = `rotate(${alpha}deg)`;
-});
